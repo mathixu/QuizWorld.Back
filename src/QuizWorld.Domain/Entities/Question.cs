a1f4839fe99514a76153028924bafc0bce0ad772
mathixu <mathieuroux71@gmail.com>
2024-04-27 18:44:39 +0200

    Question, Answer & QuestionType

---
+++ src/QuizWorld.Domain/Entities/Question.cs
@@ -0,0 +1,28 @@
+﻿using QuizWorld.Domain.Common;
+using QuizWorld.Domain.Enums;
+
+namespace QuizWorld.Domain.Entities;
+
+/// <summary>
+/// Represents a question entity.
+/// </summary>
+public class Question : BaseAuditableEntity
+{
+    /// <summary>Represents the text of the question.</summary>
+    public string Text { get; set; } = default!;
+
+    /// <summary>Represents the type of the question.</summary>
+    public QuestionType Type { get; set; }
+
+    /// <summary>Represents the answers of the question.</summary>
+    public List<Answer>? Answers { get; set; } = default!;
+
+    /// <summary>Represents the combinaisons of answers. (if type was QuestionType.Combinaison)</summary>
+    public List<List<Answer>>? Combinaisons { get; set; } = default!;
+
+    /// <summary>Represents the quiz of the question.</summary>
+    public QuizTiny Quiz { get; set; } = default!;
+
+    /// <summary>Represents the user for whom the question is customized.</summary>
+    public UserTiny? QuestionFor { get; set; }
+}
---
+++ src/QuizWorld.Domain/Enums/QuestionType.cs
@@ -0,0 +1,22 @@
+﻿namespace QuizWorld.Domain.Enums;
+
+/// <summary>
+/// Represents the type of a question.
+/// </summary>
+public enum QuestionType
+{
+    /// <summary>
+    /// Represents a simple choice question.
+    /// </summary>
+    SimpleChoice = 1,
+
+    /// <summary>
+    /// Represents a multiple choice question.
+    /// </summary>
+    MultipleChoice = 2,
+
+    /// <summary>
+    /// Represents a combinaison question.
+    /// </summary>
+    Combinaison = 3
+}
