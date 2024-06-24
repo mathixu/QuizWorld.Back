82858e7cb9f4acf203785fb01b0ed31e6a2a6880
mathixu <mathieuroux71@gmail.com>
2024-06-24 14:15:19 +0200

    GetUsersHistories

---
+++ src/QuizWorld.Application/MediatR/Sessions/Queries/GetUserHistories/GetUserHistoriesQueryHandler.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using QuizWorld.Application.Common.Models;
+using QuizWorld.Application.Interfaces.Repositories;
+using QuizWorld.Domain.Entities;
+
+namespace QuizWorld.Application.MediatR.Sessions.Queries.GetUserHistories;
+
+public class GetUserHistoriesQueryHandler(IUserHistoryRepository userHistoryRepository) : IRequestHandler<GetUserHistoriesQuery, QuizWorldResponse<PaginatedList<UserHistory>>>
+{
+    private readonly IUserHistoryRepository _userHistoryRepository = userHistoryRepository;
+
+    public async Task<QuizWorldResponse<PaginatedList<UserHistory>>> Handle(GetUserHistoriesQuery request, CancellationToken cancellationToken)
+    {
+        var histories = await _userHistoryRepository.SearchUsersAsync(request);
+
+        return QuizWorldResponse<PaginatedList<UserHistory>>.Success(histories);
+    }
+}
